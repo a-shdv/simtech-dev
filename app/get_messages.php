2b78af684d45ffcda964a188022327d6cdfb08ae
a-shdv <shadaev2001@icloud.com>
2023-10-18 05:33:52 +0400

    Update css styles

--- app/get_messages.php
+++ app/get_messages.php
@@ -9,7 +9,6 @@ $numOfRows = $query->fetch_row()[0];
 $pagesTotal = ceil($numOfRows / 5);
 $numOfMessagesDesired = 5;
 
-
 $currentPage = $_GET['page'];
 
 $query = $mysqli->prepare("SELECT * FROM form_message LIMIT " . $numOfMessagesDesired . " OFFSET " . ($currentPage * $numOfMessagesDesired));
